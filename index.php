71fb61d11e92691f13cab738763555efada4f3b1
Ambroz <killerbee8@gmail.com>
2016-07-02 00:31:37 +0200

    kilka zmian
    Dodanie Hover oraz zmieniający się tekst

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
-</head>
+
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Cempatible" content="IE=edge,chrome=1" />
 
@@ -12,7 +12,9 @@
   <link rel="stylesheet" href="style/style.css" type="text/css" />
   <link rel="stylesheet" href="css/fontello.css" type="text/css"/>
   <link href='https://fonts.googleapis.com/css?family=Lato|Josefin+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-<body>
+<script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+</head>
+<body onload>
 
   <div id="container">
     <div class="rectangle">
@@ -22,29 +24,57 @@
     </div>
 
     <div class="square">
-      <div class="tile1">1</div>
-      <div class="tile1">1</div>
+      <a href="kimjestem.php" id="kimjestem" class="tilelink">
+        <div class="tile1">
+          <i class="icon-user"></i><br />Kim Jestem?
+        </div></a>
+      <a href="cooferuje.php" id="cooferuje" class="tilelink">
+        <div class="tile1_1">
+          <i class="icon-laptop"></i><br />Co Oferuję?
+        </div></a>
       <div style="clear:both"></div>
 
-      <div class="tile2">2</div>
-      <div class="tile3">3</div>
+      <a href="cv.php" id="cv" class="tilelink">
+        <div class="tile2">
+          <i class="icon-vcard"></i><br />Curruculum Vitae
+        </div></a>
+      <a href="kontakt.php" id="kontakt" class="tilelink">
+        <div class="tile3">
+          <i class="icon-gmail"></i><br />Kontakt ze mną
+        </div></a>
       <div style="clear:both"></div>
 
-      <div class="tile4">4</div>
+      <div class="tile4">
+        <i>”Problem z programistami jest taki, że nigdy nie da się powiedzieć, co robią, zanim nie jest za późno” </i>– Seymour Cray
+      </div>
     </div>
     <div class="square">
-      <div class="tile5">5</div>
-      <div class="yt"><i class="icon-youtube"></i>
+      <div id="info" class="tile5">
+        Witam Ciebie<br /><br />
+        Witaj na mojej stronie, gdzie dowiesz się kim jestem, jakie mam zainteresowania. Zobaczysz także moje CV oraz co potrafię zrobić. Dodatkowo dowiesz sie o moich pasjach. Miłego Oglądania!
       </div>
-      <div class="fb">FB</div>
-      <div class="gplus">G+</div>
-      <div class="twitch">Twitch</div>
+      <a href="yt.php" class="tilelink">
+        <div class="yt">
+          <i class="icon-youtube"></i>
+        </div></a>
+      <!--<a href="fb.php" class="tilelink">
+        <div class="fb">
+          <i class="icon-facebook-squared"></i>
+        </div></a>
+      <a href="gplus.php" class="tilelink">
+        <div class="gplus">
+          <i class="icon-gplus-squared"></i>
+        </div></a>-->
+      <a href="twitch.php" class="tilelink">
+        <div class="twitch">
+          <i class="icon-twitch"></i>
+        </div></a>
       <div style="clear:both"></div>
     </div>
     <div style="clear:both"></div>
 
     <div class="rectangle">2016 &copy; Paweł Ambroziak - Portfolio. Programista webowy oraz Monter Filmów zaprasza do współpracy!</div>
   </div>
-
-</body>
-</html>
+<?php
+		include_once('view/footer.php');
+ ?>
---
+++ view/footer.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="js/hover.js"></script>
+
+
+</body>
+</html>
