67e759523288d8340b90b104be8932442da09d6f
Ambroz <killerbee8@gmail.com>
2016-07-01 02:53:11 +0200

    budowa

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+</head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Cempatible" content="IE=edge,chrome=1" />
+
+  <title>Paweł Ambroziak - Portfolio i Twórczość</title>
+
+  <meta name="description" content="Stworzę dla Ciebie stronę WWW oraz film! Zatrudnij Programistę: PHP, JavaScript, HTML, CSS, JQuery, MySQL, WordPress, Joomla. Zatrudnij Profesjonalny Montarz Filmu!!" />
+  <meta name="keywords" content="zamów, stronę, tworzenie, www, montarz, php, portfolio, html, javascript, jquery, WordPress, MySQL, Joomla" />
+  <link rel="stylesheet" href="style/style.css" type="text/css" />
+  <link rel="stylesheet" href="css/fontello.css" type="text/css"/>
+  <link href='https://fonts.googleapis.com/css?family=Lato|Josefin+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<body>
+
+  <div id="container">
+    <div class="rectangle">
+        <div id="logo">Paweł Ambroziak</div>
+        <div id="zegar">12:00:00</div>
+        <div style="clear:both"></div>
+    </div>
+
+    <div class="square">
+      <div class="tile1">1</div>
+      <div class="tile1">1</div>
+      <div style="clear:both"></div>
+
+      <div class="tile2">2</div>
+      <div class="tile3">3</div>
+      <div style="clear:both"></div>
+
+      <div class="tile4">4</div>
+    </div>
+    <div class="square">
+      <div class="tile5">5</div>
+      <div class="yt"><i class="icon-youtube"></i>
+      </div>
+      <div class="fb">FB</div>
+      <div class="gplus">G+</div>
+      <div class="twitch">Twitch</div>
+      <div style="clear:both"></div>
+    </div>
+    <div style="clear:both"></div>
+
+    <div class="rectangle">2016 &copy; Paweł Ambroziak - Portfolio. Programista webowy oraz Monter Filmów zaprasza do współpracy!</div>
+  </div>
+
+</body>
+</html>
